bddc3c49d6093cfe7f46e2b6079c75287fb6ed8f
lujun <779365135@qq.com>
2018-08-01 15:47:58 +0800

    n

--- src/learn/Spark/OptionFile.scala
+++ src/learn/Spark/OptionFile.scala
@@ -5,7 +5,7 @@ import java.io.File
 
 object OptionFile {
     def main(args: Array[String]): Unit = {
-        val path:File=new File("")
+        val path:File=new File("/Users/apple/Downloads/")
         for (d<-subdirs(path)){
             println(d)
         }
@@ -14,6 +14,8 @@ object OptionFile {
     //遍历目录
     def subdirs(dir:File) :Iterator[File]={
         val children=dir.listFiles.filter(_.isDirectory())
+        //  map 函数会对每一条输入进行指定的操作，然后为每一条输入返回一个对象；
+        // flatMap函数则是两个操作的集合——正是“先映射后扁平化”；先对每一条输入进行指定的操作，然后为每一条输入返回一个对象，再将所有对象合并为一个对象。
         children.toIterator++children.toIterator.flatMap(subdirs _)
     }
 
