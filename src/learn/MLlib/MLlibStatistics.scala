d9b92856f66d6ae116ed4f919af01398d7fa9eaa
lujun <779365135@qq.com>
2018-08-02 14:25:23 +0800

    n

--- src/learn/MLlib/MLlibStatistics.scala
+++ src/learn/MLlib/MLlibStatistics.scala
@@ -1,13 +1,14 @@
 package learn.MLlib
 
 //需要注意的是，默认的导入scala.collection.immutable.Vector，如果要导入spark的Vector，需要写入下式
-import org.apache.spark.ml.linalg.SparseMatrix
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
+
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.linalg.{Matrices, Matrix}
+import org.apache.spark.mllib.linalg.distributed.{RowMatrix,BlockMatrix,IndexedRowMatrix,IndexedRow,CoordinateMatrix,MatrixEntry}
 
 /**
   * Created by lj on 2018/8/1.
@@ -48,6 +49,133 @@ object MLlibStatistics {
       //Array(0,1, 3, 5)中的0表示第一个数字在0列，1示第一列有一个数字，3表示第二列有两个数字，3-1=2，最后一个数字5表示该矩阵总共有多少个数字。
       val sm1: Matrix =Matrices.sparse(3,3,Array(0,1, 3, 5), Array(1, 0,1,1,2), Array(1, 6, 8,4,5))
       println(sm1)
+
+
+      // 生产RowMatrix型矩阵,先生成一个local vector
+       val v0 = Vectors.dense(1.0, 0.0, 3.0)
+       val v1 = Vectors.sparse(3, Array(1), Array(2.5))
+       val v2 = Vectors.sparse(3, Seq((0, 1.5), (1, 1.8)))
+
+       //将local vector转换成RDD格式
+       val rows = sc.parallelize(Seq(v0, v1, v2))
+
+      //Create a RowMatrix from an RDD[Vector].
+      val mat:RowMatrix=new RowMatrix(rows)
+
+       // Get its size.
+       val m = mat.numRows()
+       val n = mat.numCols()
+
+       // QR decomposition
+       val qrResult = mat.tallSkinnyQR(true)
+
+       //计算每列之间相似度，采用抽样方法进行计算，参数为threshold；
+       val simic1=mat.columnSimilarities(0.5)
+
+       //计算每列之间相似度。
+       val simic2=mat.columnSimilarities()
+
+       //计算每列的汇总统计。
+       val simic3=mat.computeColumnSummaryStatistics()
+       println(simic3.max)
+       println(simic3.min)
+       println(simic3.mean)
+
+       //计算每列之间的协方差，生成协方差矩阵。
+        val simic4=mat.computeCovariance()
+
+       //计算格拉姆矩阵：`A^T A`。
+       //给定一个实矩阵 A，矩阵 ATA 是 A 的列向量的格拉姆矩阵，而矩阵 AAT 是 A 的行向量的格拉姆矩阵。
+       val cc2=mat.computeGramianMatrix()
+
+       //计算主成分，前K个。行为样本，列为变量。
+       val cc3=mat.computePrincipalComponents(3)
+
+       //计算矩阵的奇异值分解。
+       val svd=mat.computeSVD(4,true)
+       val U = svd.U
+       U.rows.foreach(println)
+       val s = svd.s
+       val V = svd.V
+
+
+       // 生成一个IndexedRowMatrix
+       // 生成索引行的RDD
+       val inr1=IndexedRow(1,v0)
+       val inr2=IndexedRow(2,v1)
+       val inr3=IndexedRow(3,v2)
+
+       val inrows=sc.parallelize(Array(inr1,inr2,inr3))
+
+       // Create an IndexedRowMatrix from an RDD[IndexedRow].
+       val inmat: IndexedRowMatrix = new IndexedRowMatrix(inrows)
+
+       //打印
+       inmat.rows.foreach(println)
+
+       // Get its size.
+       val inm =inmat.numRows()
+       val inn = inmat.numCols()
+
+       // Drop its row indices.
+       val rowMat: RowMatrix = inmat.toRowMatrix()
+
+
+
+       //生成CoordinateMatrix(坐标矩阵)
+       // 创建两个矩阵项ent1和ent2，每一个矩阵项都是由索引和值构成的三元组
+       val ent1 = new MatrixEntry(0,1,0.5)
+       val ent2 = new MatrixEntry(2,2,1.8)
+       // 创建RDD[MatrixEntry]
+       val entries : RDD[MatrixEntry] = sc.parallelize(Array(ent1,ent2))
+       // 通过RDD[MatrixEntry]创建一个坐标矩阵
+       val coordMat: CoordinateMatrix = new CoordinateMatrix(entries)
+       //打印
+       coordMat.entries.foreach(println)
+       //坐标矩阵可以通过transpose()方法对矩阵进行转置操作，并可以通过自带的toIndexedRowMatrix()方法转换成索引行矩阵IndexedRowMatrix。
+       // 将coordMat进行转置
+       val transMat: CoordinateMatrix = coordMat.transpose()
+       // 将坐标矩阵转换成一个索引行矩阵
+       val indexedRowMatrix = transMat.toIndexedRowMatrix()
+
+
+       //分块矩阵（Block Matrix）
+       // 创建8个矩阵项，每一个矩阵项都是由索引和值构成的三元组
+       val ment1 = new MatrixEntry(0,0,1)
+       val ment2 = new MatrixEntry(1,1,1)
+       val ment3 = new MatrixEntry(2,0,-1)
+       val ment4 = new MatrixEntry(2,1,2)
+       val ment5 = new MatrixEntry(2,2,1)
+       val ment6 = new MatrixEntry(3,0,1)
+       val ment7 = new MatrixEntry(3,1,1)
+       val ment8 = new MatrixEntry(3,3,1)
+
+
+       // 创建RDD[MatrixEntry]
+       val Bentries : RDD[MatrixEntry] = sc.parallelize(Array(ment1,ment2,ment3,ment4,ment5,ment6,ment7,ment8))
+
+       // 通过RDD[MatrixEntry]创建一个坐标矩阵
+       val BcoordMat: CoordinateMatrix = new CoordinateMatrix(Bentries)
+
+       // 将坐标矩阵转换成2x2的分块矩阵并存储，尺寸通过参数传入
+       val matA: BlockMatrix = coordMat.toBlockMatrix(2,2).cache()
+
+       // 可以用validate()方法判断是否分块成功
+       matA.validate()
+
+       //通过toLocalMatrix转换成本地矩阵，并查看其分块情况：
+       matA.toLocalMatrix
+
+       // 查看其分块情况
+       matA.numColBlocks
+       matA.numRowBlocks
+
+       // 计算矩阵A和其转置矩阵的积矩阵
+       val ata = matA.transpose.multiply(matA)
+       ata.toLocalMatrix
+
+
+
 
    }
 
--- src/learn/MLlib/readme.md
+++ src/learn/MLlib/readme.md
@@ -22,3 +22,29 @@ MLlib也支持两种类型的本地矩阵，
 表示为：[1.0, 3.0, 5.0, 2.0, 4.0, 6.0] with the matrix size (3, 2).
 
 
+MLlib 支持的分布式矩阵是一种保存有长类型的行和列指针以及double类型值的矩阵，这些矩阵存放在一个或者多个RDDs上。选择合适的格式存储大规模的矩阵至关重要的。
+毕竟在分布式机器上转换矩阵格式是非常耗费时间和内存的。MLlib目前支持四种格式的分布式矩阵。
+
+-  RowMatrix(行矩阵) ：
+
+   行矩阵RowMatrix是最基础的分布式矩阵类型。每行是一个本地向量，行索引无实际意义（即无法直接使用）。数据存储在一个由行组成的RDD中，其中每一行都使用一个本地向量来进行存储。由于行是通过本地向量来实现的，故列数（即行的维度）被限制在普通整型（integer）的范围内。在实际使用时，由于单机处理本地向量的存储和通信代价，行维度更是需要被控制在一个更小的范围之内。RowMatrix可通过一个RDD[Vector]的实例来创建
+   
+-  IndexedRowMatrix(索引行矩阵)：
+
+   索引行矩阵IndexedRowMatrix与RowMatrix相似，但它的每一行都带有一个有意义的行索引值，这个索引值可以被用来识别不同行，或是进行诸如join之类的操作。其数据存储在一个由IndexedRow组成的RDD里，即每一行都是一个带长整型索引的本地向量。
+
+-  CoordinateMatrix(坐标矩阵)：
+
+   CoordinateMatrix是由其条目的RDD支持的分布式矩阵。 每一个矩阵项MatrixEntry都是（i：Long，j：Long，value：Double）的元组，其中i是行索引，j是列索引，value是该位置的值。 只有当矩阵的两个维度都很大且矩阵非常稀疏时，才应使用CoordinateMatrix。
+
+
+-  BlockMatrix(分块矩阵):
+   
+   分块矩阵是基于矩阵块MatrixBlock构成的RDD的分布式矩阵，其中每一个矩阵块MatrixBlock都是一个元组((Int, Int), Matrix)，其中(Int, Int)是块的索引，而Matrix则是在对应位置的子矩阵（sub-matrix），其尺寸由rowsPerBlock和colsPerBlock决定，默认值均为1024。分块矩阵支持和另一个分块矩阵进行加法操作和乘法操作，并提供了一个支持方法validate()来确认分块矩阵是否创建成功。
+
+   分块矩阵可由索引行矩阵IndexedRowMatrix或坐标矩阵CoordinateMatrix调用toBlockMatrix()方法来进行转换，该方法将矩阵划分成尺寸默认为1024×1024的分块，可以在调用toBlockMatrix(rowsPerBlock, colsPerBlock)方法时传入参数来调整分块的尺寸。
+    
+    
+    
+
+
